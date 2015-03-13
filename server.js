75c98335aee35711ea21ac34f8b6d4fd55e75660
habahbeh <habahbah2007@yahoo.com>
2015-03-13 10:35:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ var server = http.createServer(function (request, response) {
   response.end("Welcome Mohammad habahbeh\n");
 });
 
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
-
-// Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8000/");
+http.listen(process.env.PORT || 3000, function(){
+  console.log('listening on', http.address().port);
+});
